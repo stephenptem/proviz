7238d5b9118ab1284b8d39d0bca91293c2d512af
Stephen Temple <stephentemple178@gmail.com>
2016-08-09 16:58:59 +0200

    Clean up

--- src/main/java/template/TemplateTab.java
+++ src/main/java/template/TemplateTab.java
@@ -13,11 +13,8 @@ import javax.swing.*;
  * @version 2016.08.09
  */
 public class TemplateTab extends OWLWorkspaceViewsTab {
-
-
     private static final long serialVersionUID = 749843192372192394L;
 
-    // private Set<ViewComponentPlugin> viewPlugins = new HashSet<>();
     private OWLEntityDisplayProvider provider = new OWLEntityDisplayProvider() {
         @Override
         public boolean canDisplay(OWLEntity owlEntity) {
@@ -60,55 +57,5 @@ public class TemplateTab extends OWLWorkspaceViewsTab {
      */
     public OWLEditorKit getOWLEditorKit() {
         return (OWLEditorKit) getWorkspace().getEditorKit();
-    }
-
-    /**
-     * Private class. Not used yet. Not sure what tasks it have. TODO
-     */
-    class NavFinder implements OWLEntityVisitor {
-
-        private String nav;
-
-        public String getNav(OWLEntity owlEntity) {
-            nav = null;
-            owlEntity.accept(this);
-            return nav;
-        }
-
-        @Override
-        public void visit(OWLClass arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void visit(OWLObjectProperty arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void visit(OWLDataProperty arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void visit(OWLNamedIndividual arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void visit(OWLDatatype arg0) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void visit(OWLAnnotationProperty arg0) {
-            // TODO Auto-generated method stub
-
-        }
     }
 }
