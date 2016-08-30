bbcba45c0d61fee49873b7480589ad852080d0d9
Stephen Temple <stephentemple178@gmail.com>
2016-08-30 11:21:19 +0200

    Copied code accross

--- src/main/java/proviz/ProvizView.java
+++ src/main/java/proviz/ProvizView.java
@@ -2,9 +2,11 @@ package proviz;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JLabel;
+import javax.swing.*;
 
 import org.apache.log4j.Logger;
+import org.pg.eti.kask.sova.utils.ReasonerLoader;
+import org.pg.eti.kask.sova.visualization.OVDisplay;
 import org.protege.editor.owl.model.selection.OWLSelectionModel;
 import org.protege.editor.owl.model.selection.OWLSelectionModelListener;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
@@ -12,17 +14,25 @@ import org.semanticweb.owlapi.model.OWLEntity;
 
 /**
  * 
- * This plugin doesn't really do much and is intended to be deleted when a developer creates his own content.
- * 
- * It really should be a AbstractOWLSelectionViewComponent but I forgot some step and it didn't work.
- * 
- * @author redmond
+ * This plugin displays is visualization
+ *
+ * @author stephen
  *
  */
 public class ProvizView extends AbstractOWLViewComponent {
 
+    // Debugging
 	private Logger logger = Logger.getLogger(ProvizView.class);
+
+    // SOVA vars
+	private OVDisplay display;
+    private boolean showFullTree = true;
+
+    // Swing vars
 	private JLabel label;
+    private JPanel panel;
+
+    // OWL vars
 	private OWLSelectionModel selectionModel;
 	private OWLSelectionModelListener listener = new OWLSelectionModelListener() {
 		
@@ -35,6 +45,32 @@ public class ProvizView extends AbstractOWLViewComponent {
 	
 	@Override
 	protected void initialiseOWLView() throws Exception {
+        ReasonerLoader.getInstance().setReasoner(getOWLModelManager().getReasoner());
+        System.out.println("załadowanie reasonera: " + getOWLModelManager().getReasoner());
+
+        if (display == null) {
+            display = new OVDisplay();
+            display.setSize(1000, 900);
+        }
+        display.generateTreeFromOWl(getOWLModelManager().getActiveOntology());
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        display.setSize(1000, 900);
+        if (leftPanel == null) {
+            initLeftPanel();
+            this.add(leftPanel);
+        }
+        if (rightPanel == null) {
+            initRightPanel();
+            this.add(rightPanel);
+        }
+
+        if (!getOWLModelManager().getReasoner().toString().toLowerCase().contains("hermit")) {
+            String message = "To use this method of visualization you need to install HermiT "
+                    + "and choose it in reasoner menu.";
+            JOptionPane.showMessageDialog(this, message);
+        }
+
+
 		logger.info("Initializing test view");
 		label = new JLabel("Hello world");
 		setLayout(new BorderLayout());
@@ -44,6 +80,7 @@ public class ProvizView extends AbstractOWLViewComponent {
 	}
 	@Override
 	protected void disposeOWLView() {
+        display.removeDisplayVis();
 		selectionModel.removeListener(listener);
 	}
 	
