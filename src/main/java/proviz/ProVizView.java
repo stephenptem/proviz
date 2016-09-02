05f380d3872bec2e229d28f7be6ddc96203c301d
Stephen Temple <stephentemple178@gmail.com>
2016-09-02 10:12:22 +0200

    Half working JUNG visualizing code

--- src/main/java/proviz/ProVizView.java
+++ src/main/java/proviz/ProVizView.java
@@ -4,24 +4,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.util.Hashtable;
+import java.util.Set;
 
 import javax.swing.*;
 
-import edu.uci.ics.jung.algorithms.layout.CircleLayout;
-import edu.uci.ics.jung.algorithms.layout.FRLayout;
-import edu.uci.ics.jung.algorithms.layout.Layout;
-import edu.uci.ics.jung.algorithms.layout.TreeLayout;
-import edu.uci.ics.jung.graph.DelegateForest;
-import edu.uci.ics.jung.graph.Forest;
-import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.algorithms.layout.*;
+import edu.uci.ics.jung.graph.*;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import org.apache.log4j.Logger;
 import org.protege.editor.owl.model.selection.OWLSelectionModel;
 import org.protege.editor.owl.model.selection.OWLSelectionModelListener;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
-import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.*;
 
 /**
  * 
@@ -42,7 +38,7 @@ public class ProVizView extends AbstractOWLViewComponent {
     private JPanel leftPanel = null, rightPanel = null;
 
     // JUNG vars
-    private Forest<Integer, String> g;
+    private DirectedSparseGraph<String, Integer> g;
 
     // OWL vars
 	private OWLSelectionModel selectionModel;
@@ -64,33 +60,54 @@ public class ProVizView extends AbstractOWLViewComponent {
 		selectionModel.addListener(listener);
 
         // Graph<V, E> where V is the type of the vertices and E is the type of the edges
-        g = new DelegateForest<Integer, String>();
+        g = new DirectedSparseGraph<String, Integer>();
 
         // Add some vertices. From above we defined these to be type Integer.
-        g.addVertex(1);
-        g.addVertex(2);
-        g.addVertex(3);
-        g.addVertex(4);
-        g.addVertex(5);
-        g.addVertex(6);
-        g.addVertex(8);
-        g.addVertex(9);
+        OWLOntology ontology = getOWLModelManager().getActiveOntology();
 
-        // Note that the default is for undirected edges, our Edges are Strings.
-        g.addEdge("Edge-A", 1, 2);
-        g.addEdge("Edge-B", 2, 3);
-        g.addEdge("Edge-C", 2, 4);
-        g.addEdge("Edge-D", 2, 5);
-        g.addEdge("Edge-E", 1, 6);
-        g.addEdge("Edge-F", 6, 7);
-        g.addEdge("Edge-G", 7, 8);
-        g.addEdge("Edge-H", 8, 9);
+        Set<OWLClass> set = ontology.getClassesInSignature(true);
+
+        // Add nodes
+        for (OWLClass c : set) {
+
+            g.addVertex(c.getIRI().getFragment());
+
+        }
+
+        // Add edges
+        int edge = 0;
+        for (OWLAxiom axiom : ontology.getAxioms()) {
+
+            if (axiom instanceof OWLSubClassOfAxiom) {
+                String subClass = null;
+                String superClass = null;
+
+                // SubClass
+                if (((OWLSubClassOfAxiom) axiom).getSubClass() instanceof OWLClass ) {
+                    subClass = ((OWLSubClassOfAxiom) axiom).getSubClass().asOWLClass().getIRI().getFragment();
+                }
+
+                // SuperClass
+                if (((OWLSubClassOfAxiom) axiom).getSuperClass() instanceof OWLClass ) {
+                    superClass = ((OWLSubClassOfAxiom) axiom).getSuperClass().asOWLClass().getIRI().getFragment();
+                }
+
+                // Add the edge between sub and super classes
+                if (subClass != null && superClass != null) {
+                    g.addEdge(edge, superClass, subClass);
+                    edge++;
+                }
+            }
+
+        }
+
 
         // Create the layout for the graph
-        TreeLayout<Integer, String> layout = new TreeLayout<Integer, String>(g);
+        DAGLayout<String, Integer> layout = new DAGLayout<String, Integer>(g);
+        layout.setSize(new Dimension(800,600));
 
         // Visualizes the graph
-        VisualizationViewer<Integer, String> vv = new VisualizationViewer<Integer, String>(layout);
+        VisualizationViewer<String, Integer> vv = new VisualizationViewer<String, Integer>(layout);
         vv.setBackground(Color.WHITE);
 
         add(vv);
