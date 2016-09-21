be0ce51cb554791156a8ce5395a082932e6dbe2b
Stephen Temple <stephentemple178@gmail.com>
2016-09-21 12:44:43 +0200

    v.1.4.2: Integrated Jeremy's and Stephen's code. Visualization now shows instances Can configure to only show up to a certain depth.

--- src/main/java/proviz/ProVizNode.java
+++ src/main/java/proviz/ProVizNode.java
@@ -1,23 +1,37 @@
 package proviz;
 
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
 
 /**
  * @author Stephen Temple
  * @version 2016.09.19
  */
 public class ProVizNode {
-    private OWLClass owlClass;
+    private OWLEntity entity;
 
-    public ProVizNode(OWLClass owlClass) {
-        this.owlClass = owlClass;
+    public ProVizNode(OWLEntity entity) {
+        this.entity = entity;
     }
 
     public OWLClass getOwlClass() {
-        return owlClass;
+        if (entity.isOWLClass()) {
+            return entity.asOWLClass();
+        } else {
+            return null;
+        }
+    }
+
+    public OWLNamedIndividual getInstance() {
+        if (entity.isOWLNamedIndividual()) {
+            return entity.asOWLNamedIndividual();
+        } else {
+            return null;
+        }
     }
 
     public String toString() {
-        return owlClass.getIRI().getFragment();
+        return entity.getIRI().getFragment();
     }
 }
--- src/main/java/proviz/ProVizView.java
+++ src/main/java/proviz/ProVizView.java
@@ -3,17 +3,12 @@ package proviz;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Point2D;
-import java.io.*;
-import java.util.Hashtable;
 import java.util.Set;
 
 import javax.swing.*;
 
 import edu.uci.ics.jung.algorithms.layout.*;
 import edu.uci.ics.jung.graph.*;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
-import edu.uci.ics.jung.visualization.Layer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
 import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
@@ -24,9 +19,8 @@ import org.protege.editor.owl.model.selection.OWLSelectionModel;
 import org.protege.editor.owl.model.selection.OWLSelectionModelListener;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
 import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.Node;
-import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+
 
 /**
  * 
@@ -35,23 +29,19 @@ import org.semanticweb.owlapi.reasoner.OWLReasoner;
  * @author stephen
  *
  */
-public class ProVizView extends AbstractOWLViewComponent {
+public class ProVizView extends AbstractOWLViewComponent implements ActionListener {
 
     // Debugging
 	private Logger logger = Logger.getLogger(ProVizView.class);
 
-    // Swing vars
-	private JLabel label;
-    private JPanel panel;
-    private JButton but3 = null;
-    private JPanel leftPanel = null, rightPanel = null;
-
     // JUNG vars
-    private DelegateTree<ProVizNode, ProVizEdge> g;
-    private VisualizationViewer<ProVizNode, ProVizEdge> vv;
+    private DelegateTree<ProVizNode, ProVizEdge> tree;
+    private VisualizationViewer<ProVizNode, ProVizEdge> viewer;
+
+    // Swing vars
+    private JSpinner depthSpinner;
 
     // OWL vars
-    private OWLReasoner reasoner;
 	private OWLSelectionModel selectionModel;
 	private OWLSelectionModelListener listener = new OWLSelectionModelListener() {
 		@Override
@@ -65,105 +55,185 @@ public class ProVizView extends AbstractOWLViewComponent {
 	protected void initialiseOWLView() throws Exception {
         logger.info("Initializing ProVizView");
 
+        // Set up display
 		setLayout(new BorderLayout());
+        JPanel menu = new JPanel(new FlowLayout());
 
+        JButton reset = new JButton("Reload");
+        reset.setActionCommand("reload");
+        reset.addActionListener(this);
+        menu.add(reset);
+
+        menu.add(new JLabel("Show depth:"));
+
+        SpinnerModel model = new SpinnerNumberModel(-1, -1, 1000, 1);
+        depthSpinner = new JSpinner(model);
+        depthSpinner.setToolTipText("Use -1 to show full tree");
+        menu.add(depthSpinner);
+
+        add(menu, BorderLayout.NORTH);
+
+        // Selection listener
 		selectionModel = getOWLWorkspace().getOWLSelectionModel();
 		selectionModel.addListener(listener);
 
-        // Graph<V, E> where V is the type of the vertices and E is the type of the edges
-        g = new DelegateTree<ProVizNode, ProVizEdge>();
-
         // Get the reasoner
-        reasoner = getOWLModelManager().getReasoner();
+        OWLReasoner reasoner = getOWLModelManager().getReasoner();
 
-        // Add root node
+        // Get root node
         ProVizNode root = new ProVizNode(reasoner.getTopClassNode().getRepresentativeElement());
-        g.setRoot(root);
 
-        // Recursively add edges
-        addChildEdges(root);
-
-        // Create the layout for the graph
-        TreeLayout<ProVizNode, ProVizEdge> layout = new TreeLayout<ProVizNode, ProVizEdge>(g);
-
-
-        // Visualizes the graph
-        vv = new VisualizationViewer<ProVizNode, ProVizEdge>(layout);
-        // White background
-        vv.setBackground(Color.WHITE);
-        // Add labels to nodes
-        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
-        // Use straight lines for edges
-        vv.getRenderContext().setEdgeShapeTransformer(EdgeShape.line(g));
-        // Add mouse controls
-        DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
-        gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
-        vv.setGraphMouse(gm);
-
-        add(vv);
+        // Visualize
+        view(root);
 	}
 	@Override
 	protected void disposeOWLView() {
 		selectionModel.removeListener(listener);
 	}
 
+    /**
+     * Handles the click events on OWL classes on the left panel
+     * @param e Class that was clicked in the hierarchy
+     */
 	private void updateView(OWLEntity e) {
+        // If selected entity is an OWL class then rebuild the tree from selected node down
 		if (e.isOWLClass()) {
-		    // If selected entity is an OWL class then rebuild the tree from selected node down
 
             // Delete the old visualization
-            remove(vv);
-
-            // Graph<V, E> where V is the type of the vertices and E is the type of the edges
-            g = new DelegateTree<ProVizNode, ProVizEdge>();
+            remove(viewer);
 
             // Add root node
             ProVizNode root = new ProVizNode(e.asOWLClass());
-            g.setRoot(root);
 
-            // Recursively add edges
-            addChildEdges(root);
-
-            // Create the layout for the graph
-            TreeLayout<ProVizNode, ProVizEdge> layout = new TreeLayout<ProVizNode, ProVizEdge>(g);
-
-
-            // Visualizes the graph
-            vv = new VisualizationViewer<ProVizNode, ProVizEdge>(layout);
-            // White background
-            vv.setBackground(Color.WHITE);
-            // Add labels to nodes
-            vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
-            // Use straight lines for edges
-            vv.getRenderContext().setEdgeShapeTransformer(EdgeShape.line(g));
-            // Add mouse controls
-            DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
-            gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
-            vv.setGraphMouse(gm);
-
-            add(vv);
+            // Visualize
+            view(root);
         }
 	}
 
-	private void addChildEdges(ProVizNode node) {
-        // Get subclasses
-        Set<OWLClass> subClasses = reasoner.getSubClasses(node.getOwlClass(), true).getFlattened();
+	/**
+	 * Displays the visualization using JUNG tree layout
+	 **/
+	private void view(ProVizNode root) {
+        // Graph<V, E> where V is the type of the vertices and E is the type of the edges
+        tree = new DelegateTree<ProVizNode, ProVizEdge>();
+        tree.setRoot(root);
 
-        // Add subclass relationships
-        for (OWLClass childClass : subClasses) {
+        // Recursively add edges
+        addAllEdges(root);
 
-            // Only add edge if the subclass isn't owl:Nothing
-            if (!childClass.getIRI().getFragment().equals("Nothing")) {
+        // Create the layout for the graph
+        TreeLayout<ProVizNode, ProVizEdge> layout = new TreeLayout<ProVizNode, ProVizEdge>(tree);
 
-                ProVizNode childNode = new ProVizNode(childClass);
+        // Visualizes the graph
+        viewer = new VisualizationViewer<ProVizNode, ProVizEdge>(layout);
+        // White background
+        viewer.setBackground(Color.WHITE);
+        // Add labels to nodes
+        viewer.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        // Use straight lines for edges
+        viewer.getRenderContext().setEdgeShapeTransformer(EdgeShape.line(tree));
+        // Add mouse controls
+        DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+        gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
+        viewer.setGraphMouse(gm);
+        viewer.repaint();
 
-                // Add child node & edge
-                g.addChild(new ProVizEdge(node, childNode), node, childNode);
+        add(viewer);
 
-                // Recurse through the child to its edges
-                addChildEdges(childNode);
+        viewer.repaint();
+    }
+
+    /**
+     * Recursively adds edges between a supplied class and all its subclasses and all instances
+     **/
+    private void addAllEdges(ProVizNode node) {
+        // Get the reasoner
+        OWLReasoner reasoner = getOWLModelManager().getReasoner();
+
+        // Continue only if depth set at -1, or if current tree depth is less than set depth
+        if (depthSpinner.getValue().equals(-1) || tree.getDepth(node) < (Integer)depthSpinner.getValue()) {
+
+            // Add instances
+            Set<OWLNamedIndividual> instances = reasoner.getInstances(node.getOwlClass(), true).getFlattened();
+            addInstances(instances, node);
+
+            // Add edges for subclasses with instances
+            Set<OWLClass> subClasses = reasoner.getSubClasses(node.getOwlClass(), true).getFlattened();
+
+            if (subClasses.isEmpty()){return;}
+
+            // Add subclass relationships
+            for (OWLClass childClass : subClasses) {
+
+                // Only add edge if the subclass isn't owl:Nothing and is satisfiable
+                if (!childClass.getIRI().getFragment().equals("Nothing") && reasoner.isSatisfiable(childClass)) {
+                    ProVizNode childNode = new ProVizNode(childClass);
+                    // Add child node & edge
+                    tree.addChild(new ProVizEdge(node, childNode), node, childNode);
+                    addAllEdges(childNode);
+                }
+
+            }
+
+        }
+    }
+
+    /**
+     * hasInstances returns true if the supplied node, or any of its children has any instances, and false otherwise
+     **/
+    private boolean hasInstances(OWLClass node){
+        // Get the reasoner
+        OWLReasoner reasoner = getOWLModelManager().getReasoner();
+
+        Set<OWLNamedIndividual> instances = reasoner.getInstances(node, true).getFlattened();
+        if (!instances.isEmpty()){
+            return true;}
+        else{
+            Set<OWLClass> subClasses = reasoner.getSubClasses(node, true).getFlattened();
+            // Check subclasses for instances
+            for (OWLClass childClass : subClasses) {
+                // Only check if the subclass isn't owl:Nothing
+                if (!childClass.getIRI().getFragment().equals("Nothing") && hasInstances(childClass)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Adds edges between the supplied node, and all its instances
+     **/
+    private void addInstances(Set<OWLNamedIndividual> instances, ProVizNode node){
+        // Get the reasoner
+        OWLReasoner reasoner = getOWLModelManager().getReasoner();
+
+        if (!instances.isEmpty()){
+            for (OWLNamedIndividual instance : instances){
+                // Only add edge if the instance isn't owl:Nothing
+                if (!instance.getIRI().getFragment().equals("Nothing")){
+                    ProVizNode instanceNode = new ProVizNode(instance);
+                    // Add instance & edge
+                    tree.addChild(new ProVizEdge(node, instanceNode), node, instanceNode);
+                }
             }
         }
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("reload")) {
+            // Delete the old visualization
+            remove(viewer);
+
+            // Get the reasoner
+            OWLReasoner reasoner = getOWLModelManager().getReasoner();
+
+            // Get root node
+            ProVizNode root = new ProVizNode(reasoner.getTopClassNode().getRepresentativeElement());
+
+            // Visualize
+            view(root);
+        }
+    }
 }
